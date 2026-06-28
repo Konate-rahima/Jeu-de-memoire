0df9da4221c37b5a9a71dbb69be16573da5a8283
konate-rahima <konaterahima@gmail.com>
2026-06-28 23:43:10 +0100

    Étape 2 - ajout logique du jeu memory

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const symbols = ["🍎", "🍌", "🍇", "🍒", "🍍", "🥝"];
 let cards = [...symbols, ...symbols];
 
+// mélange des cartes
 cards.sort(() => 0.5 - Math.random());
 
 const board = document.getElementById("gameBoard");
 
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+
+// création des cartes
 cards.forEach(symbol => {
   const card = document.createElement("div");
   card.classList.add("card");
+  card.dataset.symbol = symbol;
 
   card.innerHTML = `
     <div class="card-inner">
@@ -16,9 +23,58 @@ cards.forEach(symbol => {
     </div>
   `;
 
-  card.addEventListener("click", () => {
-    card.classList.toggle("flip");
-  });
+  // 🔥 ICI : clic correct (IMPORTANT)
+  card.addEventListener("click", flipCard);
 
   board.appendChild(card);
 });
+
+// 🔄 fonction flip
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!firstCard) {
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+
+  checkMatch();
+}
+
+// 🧠 vérification des paires
+function checkMatch() {
+  const isMatch =
+    firstCard.dataset.symbol === secondCard.dataset.symbol;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+// ✅ si match
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+// ❌ si pas match
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 900);
+}
+
+// 🔁 reset
+function resetBoard() {
+  [firstCard, secondCard, lockBoard] = [null, null, false];
+}
